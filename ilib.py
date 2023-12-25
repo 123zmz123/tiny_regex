60a8b369bbeff4daa4895b4e3f10ab92e3b1b89a
123zmz123 <1245539137@qq.com>
2023-12-25 14:20:31 +0800

    the regex engine can work for basic task

---
+++ ilib.py
@@ -0,0 +1,19 @@
+
+# stat_list = [
+#     'sA1','sA2','sA3','sA4','sA5','sA6','sA7','sA8','sA9','sA10',
+#     'sB1','sB2','sB3','sB4','sB5','sB6','sB7','sB8','sB9','sB10',
+#     'sC1','sC2','sC3','sC4','sC5','sC6','sC7','sC8','sC9','sC10',
+#     'sD1','sD2','sD3','sD4','sD5','sD6','sD7','sD8','sD9','sD10',
+#     'sE1','sE2','sE3','sE4','sE5','sE6','sE7','sE8','sE9','sE10',
+#     'sF1','sF2','sF3','sF4','sF5','sF6','sF7','sF8','sF9','sF10',
+#     'sG1','sG2','sG3','sG4','sG5','sG6','sG7','sG8','sG9','sG10',
+#     'sH1','sH2','sH3','sH4','sH5','sH6','sH7','sH8','sH9','sH10',
+#     'sI1','sI2','sI3','sI4','sI5','sI6','sI7','sI8','sI9','sI10',
+#     'sJ1','sJ2','sJ3','sJ4','sJ5','sJ6','sJ7','sJ8','sJ9','sJ10',
+# ]
+
+alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+num = ['0','1','2','3','4','5','6','7','8','9']
+
+stat_list = [a+n for a in alpha for n in  num]
+
--- impl.py
+++ impl.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from ilib import stat_list
 _EPSILON = 'ε'
 class State():
     def __init__(self,accepting=False,name=None):
@@ -40,29 +41,90 @@ class NFA():
             NfaTable={}
         if stat not in NfaTable:
             # NfaTable.update({stat:stat.name})
-            NfaTable.update({stat:stat.getEpsionClosure()})
+            NfaTable.update({stat:stat.getEpsionClosure()}) # every state at least had a ep closure
             for k in stat.transmap:
-                if k != _EPSILON:
+                if k != _EPSILON: # iterate each trans symbol
                     NfaTable[stat].update({k:stat.transmap[k]})
         for stats in stat.transmap.values():
-            for s in stats:
+            for s in stats: # iterate all states in current state transmap 
                 # NfaTable.update({s.name:s})
                 self._genNFA(s,NfaTable)
             
         return NfaTable
 
-    def _genNFA_2(self,stat:State,NfaTable=None):
-        pass
-
             
     def genNfaTable(self):
         return self._genNFA(self.inState)
-
-    def genNFATable(self):
-        pass
-        
-        
     
+
+    def genNFATable2(self):
+        s_a_map = dict()
+        raw_table = self._genNFA(self.inState)
+        new_table = {}
+        for state in raw_table:
+            s_a_map.update({state:stat_list.pop()})
+        for state in raw_table:
+            new_table.update({s_a_map[state]:{}})
+            for trans_sym in raw_table[state]:
+                trans_list = raw_table[state][trans_sym]
+                trans_st_sym_list = [s_a_map[st] for st in trans_list]
+                st_sym = s_a_map[state]
+                new_table[st_sym].update({trans_sym:trans_st_sym_list})
+        return new_table
+
+def epsilon_closure(epsilon_nfa,states):
+    ep_closure_set = set(states)
+    stack = list(states)
+    while stack:
+        current_state = stack.pop()
+        epsilon_transitions = epsilon_nfa.get(current_state,{}).get(_EPSILON,[])
+        for state in epsilon_transitions:
+            if state not in ep_closure_set:
+                ep_closure_set.add(state)
+                stack.append(state)
+    return ep_closure_set
+
+def convert_epsilon_NFA_2_NFA(epsilon_nfa):
+    nfa = {}
+    for state in epsilon_nfa:
+        nfa[state]={}
+        ep_closure_set = epsilon_closure(epsilon_nfa,[state])
+        for symbol in epsilon_nfa[state]:
+            if symbol != _EPSILON:
+                nfa[state][symbol] = epsilon_nfa[state][symbol]
+                
+        for ep_state in ep_closure_set:
+            for symbol in epsilon_nfa[ep_state].get(ep_state,{}):
+                if symbol != _EPSILON:
+                    nfa[state].setdefault(symbol,[]).extend(epsilon_nfa[ep_state][symbol])
+    return nfa
+
+def convert_epsilon_nfa_to_nfa(epsilon_nfa):
+    nfa = {}
+
+    for state in epsilon_nfa:
+        nfa[state] = {}
+
+        # Step 1: Find epsilon closure of each state
+        epsilon_closure_set = epsilon_closure(epsilon_nfa, [state])
+
+        for symbol in epsilon_nfa[state]:
+            if symbol != _EPSILON:
+                nfa[state][symbol] = epsilon_nfa[state][symbol]
+
+        # Step 2: Add transitions for epsilon closure
+        for epsilon_state in epsilon_closure_set:
+            for symbol in epsilon_nfa.get(epsilon_state, {}):
+                if symbol != _EPSILON:
+                    # nfa[state].setdefault(symbol, []).extend(epsilon_nfa[epsilon_state][symbol])
+                    nfa[state].setdefault(symbol,[])
+                    for add_state in epsilon_nfa[epsilon_state][symbol]:
+                        if add_state not in nfa[state][symbol]:
+                            nfa[state][symbol].append(add_state)
+                        
+
+
+    return nfa    
 def char(symbol:str):
     inState = State(name=symbol+"->begin")
     outState = State(accepting=True,name=symbol+"->end")
@@ -83,6 +145,7 @@ def concat(nfas:list[NFA]):
     first = nfas[0]
     for obj in nfas[1:]:
         first=concatPair(first,obj)
+    return first
 
 def orPair(first:NFA,second:NFA):
     starting = State(accepting=False,name="start")
@@ -112,14 +175,34 @@ def rep(fragment:NFA):
     accepting.addTransForSymbol(_EPSILON,fragment.inState)
     return NFA(starting,accepting)
 
+def check(input_str,nfa):
+    i=0
+    current_state='Z9'
+    while i<len(input_str):
+        symbol = input_str[i]
+        if( nfa[current_state].get(symbol) == None):
+            return False
+        current_state = nfa[current_state].get(symbol).pop()
+        i+=1
+    if nfa[current_state] == {}:
+        return True
+    else:
+        return False
 if __name__ == '__main__':
     _A = char("A")
     _B = char("B")
     _C = char("C")
     
-    pair1=orThem([_A,_B,_C])
-    
-    print(pair1.genNfaTable())
+    # pair1=concat([_C,orThem([_A,_B])])
+    # pair1 =orThem([_A,_B,_C])
+    pair1 =concat([_A,_B,_C])
+
+    e_NFA_table = pair1.genNFATable2()
+    print(e_NFA_table)
+    NFA_table =convert_epsilon_nfa_to_nfa(e_NFA_table)
+    print(NFA_table)
+    # print(NFA_table['Z9'].get('D'))
+    print(check('ABCD',NFA_table))
    # s1= State(name="s1")
     #s2=State(name="s2")
     #s1.test("haha")
